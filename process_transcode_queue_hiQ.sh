d1060eff19e8d606e2f9ba1f44d448a1ec13a4c9
frankjschultze <frank@dovecor.com>
2016-11-28 21:20:14 +0200

    threads 0

--- process_transcode_queue_hiQ.sh
+++ process_transcode_queue_hiQ.sh
@@ -13,19 +13,18 @@ filename="${filename%.*}"
 
 if [ "$short" != "._" ]; then
 	#r128 audio
-	ffmpeg -nostats -threads 192 -i "$1/$2" -vn -filter_complex ebur128 -f null - 2> "$TMPDIR/r128.txt"
+	ffmpeg -nostats -threads 0 -i "$1/$2" -vn -filter_complex ebur128 -f null - 2> "$TMPDIR/r128.txt"
 	line=$(tail "$TMPDIR/r128.txt" | grep I:)
 	read -r _ r128 _<<< "$line"
 
 	correct=$(echo "(-23)-($r128)" | bc)
 	correct="${correct}dB"
 
-	ffmpeg -threads 192 -y -i "$1/$2" -af "volume=$correct" "/home/ingest/out_qa/${filename}_r128.${extension}"
+	ffmpeg -threads 0 -y -i "$1/$2" -af "volume=$correct" "/home/ingest/out_qa/${filename}_r128.${extension}"
 	sleep 2
 
-	#ffmpeg -threads 192 -y -i "/usr/share/nginx/html/upload/server/php/files/out_qa/${filename}_r128.${extension}" -r 25 -c:v libx264 -vf scale=1920:1080 -preset medium -crf 10 -c:a libfdk_aac -b:a 128k -ar 48000 -pass 1 -f mp4 /dev/null && \
-	ffmpeg -threads 192 -y -i "/home/ingest/out_qa/${filename}_r128.${extension}" -r 25 -c:v libx264 -vf scale=1920:1080 -preset slow -crf 10 -c:a libfdk_aac -b:a 128k -ar 48000 "/home/ingest/out_qa/${filename}_hiq.${extension}" 
-
+	#ffmpeg -threads 0 -y -i "/usr/share/nginx/html/upload/server/php/files/out_qa/${filename}_r128.${extension}" -r 25 -c:v libx264 -vf scale=1920:1080 -preset medium -crf 10 -c:a libfdk_aac -b:a 128k -ar 48000 -pass 1 -f mp4 /dev/null && \
+	ffmpeg -threads 0 -y -i "/home/ingest/out_qa/${filename}_r128.${extension}" -r 25 -c:v libx264 -vf scale=1920:1080 -preset slow -crf 10 -c:a libfdk_aac -b:a 128k -ar 48000 "/home/ingest/out_qa/${filename}_hiq.${extension}" 
 	#move to processed
 	rm -rf "/home/ingest/out_qa/${filename}_r128.${extension}"
 	sleep 1
